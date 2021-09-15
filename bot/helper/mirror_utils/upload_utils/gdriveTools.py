6d2eb9f84bea2bbdc2f2cc9aa0747220292930bc
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-09-15 12:03:46 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -130,7 +130,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirrorbot',
+            'description': 'Uploaded using C2P tech bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -189,7 +189,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirrorbot',
+            'description': 'Uploaded by C2P TECH bot',
             'mimeType': mime_type,
         }
         try:
@@ -587,9 +587,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirrorbot Search',
-                                 author_name='Slam Mirrorbot',
-                                 author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                 title = 'C2P Tech bot Search',
+                                 author_name='Aashath',
+                                 author_url='https://www.youtube.com/channel/UCJVrPRNqzGPQfG57H-vgz0w',
                                  html_content=content)
         return
 
@@ -772,9 +772,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                    title = 'Slam Mirrorbot Search',
-                                                    author_name='Slam Mirrorbot',
-                                                    author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                                    title = 'C2P tech bot Search',
+                                                    author_name='Aashath',
+                                                    author_url='https://www.youtube.com/channel/UCJVrPRNqzGPQfG57H-vgz0w',
                                                     html_content=content
                                                     )['path'])
 
